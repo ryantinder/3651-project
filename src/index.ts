8fa4491530c4a4da440d955f924f5814d9cd3d5c
ryantinder <ryantinder56@gmail.com>
2024-04-10 12:35:50 -0400

    init

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log(Bun.env)
+import express from "express";
+
+const app = express();
+
+interface Price {
+    ticker: string;
+    price: number;
+}
+
+app.use(express.json());
+
+app.get("/tickers", async (req, res) => {})
+
+app.get("/:ticker", async (req, res) => {
+    const { ticker } = req.params;
+    console.log("fetching price for " + ticker)
+    res.status(200).json({ ticker: ticker, price: 19.65});
+})
+
+app.listen(process.env.PORT || 3333, () => {
+    console.log("Server running on port 3333")
+})
